26316e0e4063543a3bbf8ea7d2c434c1d0ad2f6a
Luan Araujo <luaanscar@gmail.com>
2024-02-08 23:31:20 -0300

    "WIP"

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default async function Home() {
     return (
         <>
-            <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
+            <section className="h-full w-full md:pt-96 mt-[-70px] relative flex items-center justify-center flex-col ">
                 <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10">
                 </div>
 
