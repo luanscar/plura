27337fce3f2b061f51087a6eec61b0e55ab247ce
Luan Araujo <luaanscar@gmail.com>
2024-02-22 00:20:05 -0300

    https://youtu.be/6omuUOZcWL0?t=17634

--- src/lib/queries.ts
+++ src/lib/queries.ts
@@ -231,6 +231,21 @@ export const initUser = async (newUser: Partial<User>) => {
   return userData;
 };
 
+export const updateUser = async (user: Partial<User>) => {
+  const response = await db.user.update({
+    where: { email: user.email },
+    data: { ...user },
+  })
+
+  await clerkClient.users.updateUserMetadata(response.id, {
+    privateMetadata: {
+      role: user.role || 'SUBACCOUNT_USER',
+    },
+  })
+
+  return response
+}
+
 export const upsertAgency = async (agency: Agency, price?: Plan) => {
   if (!agency.companyEmail) return null;
   try {
@@ -381,5 +396,14 @@ export const upsertSubAccount = async (subAccount: SubAccount) => {
       },
     },
   });
+  return response;
+};
+
+export const getUserPermissions = async (userId: string) => {
+  const response = await db.user.findUnique({
+    where: { id: userId },
+    select: { Permissions: { include: { SubAccount: true } } },
+  });
+
   return response;
 };
